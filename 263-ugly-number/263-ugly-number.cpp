2f37df82a45f07592fd031c35eec708fff256a4d
amanmanwani00 <104587833+amanmanwani00@users.noreply.github.com>
2022-04-30 22:01:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (87.79%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isUgly(int n)
+    {
+        if(n==0)
+        {
+            return false;
+        }
+        if(n%2==0)
+        {
+            while(n%2==0)
+            {
+                n=n/2;
+            }
+        }
+        if(n%3==0)
+        {
+            while(n%3==0)
+            {
+                n=n/3;
+            }
+        }
+        if(n%5==0)
+        {
+            while(n%5==0)
+            {
+                n=n/5;
+            }
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return false;
+        
+        
+    }
+};
