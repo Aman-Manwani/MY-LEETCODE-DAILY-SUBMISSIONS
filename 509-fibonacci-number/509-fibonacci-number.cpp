5a14d0943f1d0410f9d4d676195758b1937f14d2
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-07-25 14:45:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int ans(int n,vector<int>&dp)
+    int fib(int n) 
     {
         if(n<=1)
         {
             return n;
         }
-        if(dp[n]!=-1)
+        int prev1=0;
+        int prev2=1;
+        int cur;
+        for(int i=2;i<=n;i++)
         {
-            return dp[n];       
+            cur=prev1+prev2;
+            prev1=prev2;
+            prev2=cur;
         }
-        return dp[n]=ans(n-1,dp)+ans(n-2,dp);
-    }
-    int fib(int n) 
-    {
-        vector<int>dp(n+1,-1);
-        return ans(n,dp);       
+        return cur;
     }
 };
