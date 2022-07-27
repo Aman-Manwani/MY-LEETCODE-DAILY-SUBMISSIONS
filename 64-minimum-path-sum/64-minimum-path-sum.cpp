6b9c49091f38ae5d35c60075291b6512004f2656
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-07-28 00:34:58 +0530

    Time: 7 ms (95.64%), Space: 10 MB (52.10%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int ans(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp)
+    /*int ans(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp)
     {
         //base case
         if(i==m-1 && j==n-1)
@@ -20,12 +20,35 @@ public:
         int op2=grid[i][j]+ans(i,j+1,m,n,grid,dp);
         cout<<op1<<" "<<op2;
         return dp[i][j]= min(op1,op2);
-    }
+    }*/
     int minPathSum(vector<vector<int>>& grid) 
     {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return ans(0,0,m,n,grid,dp); 
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0) dp[i][i]=grid[i][j];
+                else
+                {
+                    int op1=grid[i][j];
+                    if(i>0)
+                    {
+                        op1+=dp[i-1][j];
+                    }else
+                        op1+=1000;
+                    int op2=grid[i][j];
+                    if(j>0)
+                    {
+                        op2+=dp[i][j-1];
+                    }else
+                        op2+=1000;
+                    dp[i][j]=min(op1,op2);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }; 
