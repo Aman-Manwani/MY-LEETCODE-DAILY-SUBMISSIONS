eaaddfc13396bfdcad71d36f6154c604d95bffee
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-07 00:15:54 +0530

    Time: 12 ms (81.90%), Space: 9.2 MB (21.81%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,17 +3,17 @@ public:
     int ans(int i,int j,string &s,string &t,vector<vector<int>>&dp)
     {
         //base case
-        if(i<0)     return j+1;
-        if(j<0)     return i+1;
+        if(i==0)     return j;
+        if(j==0)     return i;
         if(dp[i][j]!=-1)    return dp[i][j];
         //rec case
-        if(s[i]==t[j])  return dp[i][j] = ans(i-1,j-1,s,t,dp);
+        if(s[i-1]==t[j-1])  return dp[i][j] = ans(i-1,j-1,s,t,dp);
         else
             return dp[i][j] = min(1+ans(i-1,j,s,t,dp),min(1+ans(i,j-1,s,t,dp),1+ans(i-1,j-1,s,t,dp)));
     }
     int minDistance(string s, string t) 
     {
         vector<vector<int>>dp(s.length()+1,vector<int>(t.length()+1,-1));
-        return ans(s.length()-1,t.length()-1,s,t,dp);
+        return ans(s.length(),t.length(),s,t,dp);
     }
 };
