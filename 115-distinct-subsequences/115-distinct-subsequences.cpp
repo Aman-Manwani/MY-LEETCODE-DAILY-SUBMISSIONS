a0a56ed44a084cd5151aec1c6057d578c0651a9b
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-06 14:46:49 +0530

    Time: 80 ms (27.67%), Space: 18.6 MB (48.68%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,14 +3,14 @@ public:
     int ans(int i,int j,string &s,string &t,vector<vector<int>>&dp)
     {
         //base case
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
         //rec case
-        if(s[i]==t[j])  return dp[i][j] = ans(i-1,j-1,s,t,dp)+ans(i-1,j,s,t,dp);
+        if(s[i-1]==t[j-1])  return dp[i][j] = ans(i-1,j-1,s,t,dp)+ans(i-1,j,s,t,dp);
         else
             return dp[i][j] = ans(i-1,j,s,t,dp);
     }
@@ -19,6 +19,6 @@ public:
         int n=s.length();
         int m=t.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return ans(n-1,m-1,s,t,dp);
+        return ans(n,m,s,t,dp);
     }
 };
