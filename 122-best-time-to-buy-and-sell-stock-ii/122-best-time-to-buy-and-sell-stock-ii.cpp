5eea0c8cf55c0110b8b1b1b2529da380bdddf78e
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-09 22:40:38 +0530

    Time: 27 ms (7.50%), Space: 17.3 MB (8.87%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans(int index,int buy,vector<int>&prices,vector<vector<int>>&dp)
+    {
+        //base case
+        if(index==prices.size())
+        {
+            return 0;
+        }
+        if(dp[index][buy]!=-1)  return dp[index][buy];
+        //rec case
+        long long profit=0;
+        if(buy==1)
+        {
+            profit=max(-prices[index]+ans(index+1,0,prices,dp),0+ans(index+1,1,prices,dp));
+        }else
+            profit=max(prices[index]+ans(index+1,1,prices,dp),0+ans(index+1,0,prices,dp));
+        return dp[index][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices)
+    {
+        vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
+        return ans(0,1,prices,dp);
+    }
+};
