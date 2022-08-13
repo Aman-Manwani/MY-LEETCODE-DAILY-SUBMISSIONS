b69ae4c93c27d1176c17bf7e91c91593174203d4
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-13 23:45:54 +0530

    Time: 970 ms (16.03%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -15,8 +15,8 @@ public:
             len=max(len,1+ans(index+1,nums,index,dp));
         }
         return dp[index][prev+1] = len;
-    }*/
-    int lengthOfLIS(vector<int>& nums) 
+    }
+    int ans2(vector<int>&nums)
     {
         vector<int>temp;
         temp.push_back(nums[0]);
@@ -34,5 +34,23 @@ public:
             }        
         }
         return len;
+    }*/
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int prev=index-1;prev>=-1;prev--)
+            {
+                int len=0+dp[index+1][prev+1];
+                if(prev==-1 || nums[index]>nums[prev])
+                {
+                    len=max(len,1+dp[index+1][index+1]);
+                }
+                dp[index][prev+1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
