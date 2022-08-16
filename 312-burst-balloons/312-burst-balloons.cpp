380df6f3eb8df8200e0d6cbad763be9b2545cdb8
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-16 12:40:07 +0530

    Time: 368 ms (88.71%), Space: 10.4 MB (52.38%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int ans(int start,int end,vector<int>&nums,vector<vector<int>>&dp)
+    /*int ans(int start,int end,vector<int>&nums,vector<vector<int>>&dp)
     {
         //base case
         if(start>end)   return 0;
@@ -13,12 +13,26 @@ public:
             maxi=max(maxi,cost);
         }
         return dp[start][end] = maxi;
-    }
+    }*/
     int maxCoins(vector<int>& nums)
     {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
-        return ans(1,nums.size()-2,nums,dp);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0));
+        for(int start=nums.size()-2;start>=1;start--)
+        {
+            for(int end=1;end<=nums.size()-2;end++)
+            {
+                if(start>end)   continue;
+                int maxi=INT_MIN;
+                for(int i=start;i<=end;i++)
+                {
+                    int cost=nums[start-1]*nums[i]*nums[end+1]+dp[start][i-1]+dp[i+1][end];
+                    maxi=max(maxi,cost);
+                }
+                dp[start][end] = maxi;
+            }
+        }
+        return dp[1][nums.size()-2];
     }
 };
