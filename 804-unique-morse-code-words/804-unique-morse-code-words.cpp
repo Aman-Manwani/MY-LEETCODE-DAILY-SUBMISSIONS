a6cb78eff6fd5ee9bf108ef329422f7353d86a29
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-17 09:25:50 +0530

    Time: 4 ms (71.25%), Space: 8.8 MB (11.70%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -4,12 +4,18 @@ public:
     {
         unordered_set<string>s;
         vector<string>temp = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_map<char,string>m;
+        for(char i='a';i<='z';i++)
+        {
+            m[i]=temp[i-'a'];
+        }
+        cout<<m.size();
         string ans="";
         for(int i=0;i<nums.size();i++)
         {
             for(int j=0;j<nums[i].size();j++)
             {
-                ans+=temp[nums[i][j]-'a'];
+                ans+=m[nums[i][j]];
             }
             s.insert(ans);
             ans="";
