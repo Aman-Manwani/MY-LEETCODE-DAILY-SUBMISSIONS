f5c7392982ff280cee7ccc91ba7d9f68f1530680
AMAN MANWANI <104587833+amanmanwani00@users.noreply.github.com>
2022-08-25 16:20:30 +0530

    Time: 810 ms (10.24%), Space: 9.2 MB (51.87%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int ans(int n,vector<int>&dp)
+    /*int ans(int n,vector<int>&dp)
     {
         //base case
         if(n<=3)
@@ -15,12 +15,24 @@ public:
             mini=min(mini,1+ans(n-i*i,dp));
         }
         return dp[n] = mini;
-    }
+    }*/
     int numSquares(int n) 
     {
         int a=sqrt(n);
         if(a*a==n)  return 1;
-        vector<int>dp(n+1,-1);
-        return ans(n,dp);
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                dp[i]=min(dp[i],1+dp[i-j*j]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<dp[i]<<" ";
+        }
+        return dp[n];
     }
 };
