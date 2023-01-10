7a1536efd713cb56fe3f5959f6540729500473c5
AMAN MANWANI <104587833+Aman-Manwani@users.noreply.github.com>
2023-01-10 21:38:01 +0530

    Time: 4 ms (32.59%), Space: 6.3 MB (19.81%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int ans(int i,int n,string temp)
+    int ans(int i,int n,vector<vector<int>>&dp)
     {
         //base case
         if(n==0)    return 1;
         if(i>=5)    return 0;
+        if(dp[i][n]!=-1)
+        {
+            return dp[i][n];
+        }
         //rec case
         // pick
-        int pick = ans(i,n-1,temp);
+        int pick = ans(i,n-1,dp);
         //not pick
-        int not_pick = ans(i+1,n,temp);
-        return pick + not_pick;
+        int not_pick = ans(i+1,n,dp);
+        return dp[i][n] = pick + not_pick;
     }
     int countVowelStrings(int n) 
     {
+        vector<vector<int>>dp(5,vector<int>(n+1,-1));
         string temp="aeiou";
-        return ans(0,n,temp);
+        return ans(0,n,dp);
     }
 };
