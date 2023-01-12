8e745d41e83cd5a9e7e101b2eeb54127eafee725
AMAN MANWANI <104587833+Aman-Manwani@users.noreply.github.com>
2023-01-13 01:16:38 +0530

    Time: 4 ms (76.24%), Space: 9.7 MB (33.56%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -29,6 +29,24 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) 
     {
         vector<int>dp(days.size()+1,-1);
-        return solve(days,costs,0,dp);
+        // return solve(days,costs,0,dp);
+        int n=days.size();
+        dp[n]=0;
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            int op1 = costs[0] + dp[idx+1];
+        
+            int i;
+            for(i=idx ; i<days.size() && days[i]<days[idx]+7;i++);
+
+            int op2 = costs[1]+dp[i];
+
+
+            for(i=idx ; i<days.size() && days[i]<days[idx]+30;i++);
+
+            int op3 = costs[2]+dp[i];
+            dp[idx]=min(op1,min(op2,op3));
+        }
+        return dp[0];
     }
 };
