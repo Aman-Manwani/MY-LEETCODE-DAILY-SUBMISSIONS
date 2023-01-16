3530e3fbb2032c1054c802ca0ae47069ff375e62
AMAN MANWANI <104587833+Aman-Manwani@users.noreply.github.com>
2023-01-16 11:04:03 +0530

    Time: 35 ms (34.36%), Space: 17.1 MB (78.65%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& temp) {
+        nums.push_back(temp);
+        vector<vector<int>>v;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            int mini=nums[i][0],maxi=nums[i][1];
+            i++;
+            while(i<nums.size() && ((nums[i][0]>=mini && nums[i][0]<=maxi) || (nums[i][1]>=mini && nums[i][1]<=maxi)) )
+            {
+                mini=min(mini,nums[i][0]);
+                maxi=max(maxi,nums[i][1]);
+                i++;
+            }
+            i--;
+            v.push_back({mini,maxi});
+        }
+        for(int i=0;i<v.size();i++)
+        {
+            cout<<v[i][0]<<" "<<v[i][1]<<endl;
+        }
+        return v;
+    }
+};
