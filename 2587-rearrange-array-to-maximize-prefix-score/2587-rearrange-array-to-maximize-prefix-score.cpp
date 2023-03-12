014e0bbd174cdb4018a8e31da7c6ef3077711253
AMAN MANWANI <104587833+Aman-Manwani@users.noreply.github.com>
2023-03-12 12:42:36 +0530

    Time: 1008 ms (25.00%), Space: 83.3 MB (25.00%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    int maxScore(vector<int>& nums)
+    int maxScore(vector<int>& nums) 
     {
-        bool pos = false;
-        int no_pos=0,no_zero=0;
-        long long int sum=0;
+        sort(nums.begin(),nums.end(),greater<int>());
+        long long int sum=INT_MAX,ans=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]>0)
+            if(sum==INT_MAX && nums[i]>0)
             {
-                pos=true;
-                no_pos++;
-                sum+=nums[i];
+                sum=nums[i];
+                ans++;
+                continue;
             }
-            if(nums[i]==0)
+            if(sum==INT_MAX)
             {
-                no_zero++;
+                return ans;
             }
-        }
-        if(pos==false || no_pos==0)
-        {
-            return 0;
-        }
-        int ans = no_pos;
-        sort(nums.begin(),nums.end());
-        int index=nums.size()-no_pos-1;
-        while(index>=0 && sum>abs(nums[index]))
-        {
-            sum=sum+nums[index];
-            index--;
-            ans++;
-            cout<<ans<<sum<<endl;
+            sum+=nums[i];
+            if(sum<=0)
+            {
+                return ans;
+            }else
+                ans++;
+            cout<<ans<<" "<<sum<<endl;
         }
         return ans;
     }
